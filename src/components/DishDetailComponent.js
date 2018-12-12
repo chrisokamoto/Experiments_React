47c7802920b1f19676a9736d036fc8a84baf8da4
Chris Okamoto <christiane.okamoto@gmail.com>
2018-12-12 16:39:52 +0100

    post comments to server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -30,7 +30,7 @@ class CommentForm extends Component {
 
   submitComment(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.author, values.comment)
+    this.props.postComment(this.props.dishId, values.rating, values.author, values.comment)
   }
 
   render() {
@@ -112,7 +112,7 @@ function RenderDish({dish}) {
     );
 }
 
-function RenderComments({comments, addComment, dishId}) {
+function RenderComments({comments, postComment, dishId}) {
   if(comments != null)
     return(
       <div className="col-12 col-md-5 m-1">
@@ -129,7 +129,7 @@ function RenderComments({comments, addComment, dishId}) {
             )
           })}
         </ul>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     )
   else
@@ -172,7 +172,7 @@ const DishDetail = (props) => {
             <hr />
           </div>
           <RenderDish dish={props.dish} />
-          <RenderComments comments={props.comments} addComment={props.addComment}
+          <RenderComments comments={props.comments} postComment={props.postComment}
             dishId={props.dish.id} />
         </div>
       </div>
