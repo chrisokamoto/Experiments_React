ee49b826b82e1f661196fb3faa94b7d3903f8ac4
Chris Okamoto <christiane.okamoto@gmail.com>
2018-12-12 15:11:49 +0100

    fetch infos from json-server

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,5 +1,6 @@
 import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../shared/dishes'
+import { baseUrl } from '../shared/baseUrl'
 
 export const addComment = (dishId, rating, author, comment) => ({
   type: ActionTypes.ADD_COMMENT,
@@ -14,21 +15,59 @@ export const addComment = (dishId, rating, author, comment) => ({
 export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
 
-  setTimeout(() => {
-    dispatch(addDishes(DISHES));
-  }, 2000);
-}
+  return fetch(baseUrl + 'dishes')
+  .then(response => response.json()) // convert response to json
+  .then(dishes => dispatch(addDishes(dishes))); // the response is renamed to dishes
+};
+
+export const fetchComments = () => (dispatch) => {
+  return fetch(baseUrl + 'comments')
+  .then(response => response.json())
+  .then(comments => dispatch(addComments(comments)));
+};
+
+export const commentsFailed = (errMess) => ({
+  type: ActionTypes.COMMENTS_FAILED,
+  payload: errMess
+});
+
+export const addComments = (comments) => ({
+  type: ActionTypes.ADD_COMMENTS,
+  payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => {
+  dispatch(promosLoading());
+
+  return fetch(baseUrl + 'promotions')
+  .then(response => response.json())
+  .then(promos => dispatch(addPromos(promos)));
+};
+
+export const promosLoading = () => ({
+  type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errMess) => ({
+  type: ActionTypes.PROMOS_FAILED,
+  payload: errMess
+});
+
+export const addPromos = (promotions) => ({
+  type: ActionTypes.ADD_PROMOS,
+  payload: promotions
+});
 
 export const dishesLoading = () => ({
   type: ActionTypes.DISHES_LOADING
-})
+});
 
 export const dishesFailed = (errMess) => ({
   type: ActionTypes.DISHES_FAILED,
   payload: errMess
-})
+});
 
 export const addDishes = (dishes) => ({
   type: ActionTypes.ADD_DISHES,
   payload: dishes
-})
+});
